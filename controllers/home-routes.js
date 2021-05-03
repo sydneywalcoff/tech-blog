73ea0a35e489212af39101181cbedbc86ae8d918
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-05-03 15:35:47 -0700

    homepage renders correctly

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,7 +4,24 @@ const { Post, User, Comment } = require('../models/index');
 
 // homepage
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: ['id', 'title', 'body_content', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'content', 'post_id'],
+                include: [{
+                    model: User,
+                    attributes: ['username']
+                }]
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts });
+    })
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
