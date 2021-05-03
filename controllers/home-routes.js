9dc3d97a74c593c0f7bac20f486f2275860a35ce
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-05-03 15:21:30 -0700

    main view and homepage view

---
+++ controllers/home-routes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models/index');
+
+// homepage
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
