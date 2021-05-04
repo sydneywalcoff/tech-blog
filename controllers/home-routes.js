51df0aa69e09fa31e878ed3384e560e77998ad46
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-05-04 12:30:32 -0700

    started single-post route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,4 +34,44 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+// single post
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'body_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', {  post });
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
