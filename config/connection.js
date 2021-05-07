64f155a61f526021a390bb5f621f46acab164907
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-05-07 11:00:02 -0700

    fix stupid typo in jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if(process.env.JAWSBD_URL) {
-    sequelize = new Sequelize(process.env.JAWSBD_URL);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
